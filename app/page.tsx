94ae49e72436ab4b637fdcdf8d41bba3ffffad80
James Jegede <jexzyjames@gmail.com>
2026-09-06 01:39:11 +0100

    Add new components to the Home page

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,11 @@ import LearningMgt from "./components/LearningMgt";
 import Hero from "./components/Hero";
 import CorporateTraining from "./components/TrainingPrograms";
 import ManagementProgram from "./components/ManagementProgram";
+import CallCTA from "./components/CallCTA";
+import LearningWithCeo from "./components/LearningWIthCeo";
+import ConsultantsTtraining from "./components/ConsultantsTraining";
+import CallToConsult from "./components/CallToConsult";
+import Testimonials from "./components/Testimonials";
 export default function Home() {
 
   return (
@@ -11,6 +16,11 @@ export default function Home() {
     <LearningMgt/>
     <CorporateTraining/>
     <ManagementProgram/>
+     <LearningWithCeo/>
+    <ConsultantsTtraining/>
+    <CallToConsult />
+    <Testimonials />
+    <CallCTA/>
 </>
   );
 }
