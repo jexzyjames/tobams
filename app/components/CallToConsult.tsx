444e6038ae5e6f11e0c89ed526af3016e225707c
James Jegede <jexzyjames@gmail.com>
2026-09-06 21:44:32 +0100

    Adjust padding for CallToConsult component
    Updated padding for responsive design on CallToConsult component.

--- app/components/CallToConsult.tsx
+++ app/components/CallToConsult.tsx
@@ -3,7 +3,7 @@
 export default function CallToConsult(){
     return (
 
-        <div className="bg-user items-center mx-8 lg:mx-auto flex flex-col gap-6  justify-center mt-5 px-16 py-8 rounded-[8px]">
+        <div className="bg-user items-center mx-8 lg:mx-auto flex flex-col gap-6  justify-center mt-5  md:px-16 py-8 rounded-[8px]">
             <p className="hidden lg:block text-center text-white font-semibold tracking-[3%] text-[20px]">Want to accelerate professional growth and development at your organisation?See how we can help.</p>
             <p className=" lg:hidden text-center text-white font-semibold text-[16px] ">
                 Don't just dream it—let's build it! Click now and start your project with Tobams Group. Your journey to digital excellence begins here.
